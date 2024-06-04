6b1b092647b4f4236ca784e55c4fc5b1e01c0d9f
Amarpreet Singh <Amarpreet.A.Singh@ibm.com>
2024-06-04 14:48:55 -0400

    If no 4K pages, use smallest one as per omrvmem_startup
    Co-authored-by: Tobi Ajila <atobia@ca.ibm.com>
    Co-authored-by: Marius Pirvu <mpirvu@ca.ibm.com>
    Co-authored-by: Nathan Henderson <nathan.henderson@ibm.com>
    Signed-off-by: Amarpreet Singh <Amarpreet.A.Singh@ibm.com>

--- port/common/omrmem32helpers.c
+++ port/common/omrmem32helpers.c
@@ -532,6 +532,7 @@ allocateVmemRegion32(struct OMRPortLibrary *portLibrary, uintptr_t byteAmount, J
 	 */
 #if defined(OMR_PORT_CAN_RESERVE_SPECIFIC_ADDRESS)
 	J9PortVmemIdentifier *vmemID = NULL;
+	uintptr_t *pageSizes = NULL;
 	uintptr_t pageSize = 0;
 	uintptr_t i = 0;
 	J9HeapWrapper *wrapper = NULL;
@@ -556,8 +557,10 @@ allocateVmemRegion32(struct OMRPortLibrary *portLibrary, uintptr_t byteAmount, J
 		return NULL;
 	}
 
+	pageSizes = portLibrary->vmem_supported_page_sizes(portLibrary);
+
 	/* get the default page size */
-	pageSize = portLibrary->vmem_supported_page_sizes(portLibrary)[0];
+	pageSize = pageSizes[0];
 	if (0 == pageSize) {
 		Trc_PRT_mem_allocate_memory32_failed_page(callSite);
 		portLibrary->mem_free_memory(portLibrary, vmemID);
@@ -570,7 +573,15 @@ allocateVmemRegion32(struct OMRPortLibrary *portLibrary, uintptr_t byteAmount, J
 	// 	pageSize = 0x1000;
 	// }
 	/* Make sure that we only allocate 4k pages. */
-	pageSize = 0x1000;
+	// pageSize = 0x1000;
+	/* If 4K is not the default page size, then look for the smallest one as per omrvmem_startup. */
+	if (0x1000 != pageSize) {
+		for (i = 0 ; pageSizes[i] != 0 ; i++) {
+			if (pageSize > pageSizes[i]) {
+				pageSize = pageSizes[i];
+			}
+		}
+	}
 
 #if defined(LINUX)
 		if (OMR_ARE_ALL_BITS_SET(PPG_mem32BitFlags, OMRPORT_MEM_32BIT_FLAGS_TMP_FILE_BACKED_VMEM)) {
